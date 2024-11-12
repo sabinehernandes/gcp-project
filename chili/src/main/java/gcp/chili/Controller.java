ad5cf823c8902704229c4b26f96ccc0b1905581b
Sabine <sabinehernandes@gmail.com>
2024-11-12 13:29:08 +0100

    add controller with get endpoint git commit -m “$1
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

---
+++ chili/src/main/java/gcp/chili/Controller.java
@@ -0,0 +1,18 @@
+package gcp.chili;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class Controller {
+
+    String greeting = "Hello";
+
+    @GetMapping("localhost:8080/{name}")
+    public String getGreeting(@PathVariable String name) {
+        return greeting + " " + name;
+    }
+}
