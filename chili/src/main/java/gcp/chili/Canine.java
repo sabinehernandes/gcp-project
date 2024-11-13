a5e306ff3de593d90f8b7f25a497c564e049c9f9
TibbersEvolved <erik.olin.spanga@gmail.com>
2024-11-13 11:37:58 +0100

    Created canine entity
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

---
+++ chili/src/main/java/gcp/chili/Canine.java
@@ -0,0 +1,63 @@
+package gcp.chili;
+
+import jakarta.persistence.*;
+import org.springframework.lang.Nullable;
+
+@Entity
+public class Canine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+    private String breed;
+    private int age;
+    @Column(nullable = true)
+    private String imageUrl;
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Canine(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Canine() {
+    }
+}
