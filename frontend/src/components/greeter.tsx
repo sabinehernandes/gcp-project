18d0c1bf250d0ef63027888de476eb2712a3d755
AndreasErikLundmark <andreas.erik.lundmark@gmail.com>
2024-11-13 09:44:27 +0100

    more form
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Erik Olin <erik.olin.spanga@gmail.com>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

--- frontend/src/components/greeter.tsx
+++ frontend/src/components/greeter.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
 export default function Greeter() {
+  const [name, setName] = useState("Hernandes");
+
   return (
     <>
       <form>
         <label>
           Greeting:
-          <input type="text" name="name"></input>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></input>
         </label>
         <button type="submit">Some text just too</button>
       </form>
