e730bc32533b68154c2b38606b1e6e5664924506
AndreasErikLundmark <andreas.erik.lundmark@gmail.com>
2024-11-13 09:37:44 +0100

    form
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Erik Olin <erik.olin.spanga@gmail.com>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Greeter from "./components/greeter";
 
 function App() {
   return (
     <>
       <h1>Chilis Greeting Service @TM</h1>
+      <Greeter />
     </>
   );
 }
---
+++ frontend/src/components/greeter.tsx
@@ -0,0 +1,13 @@
+export default function Greeter() {
+  return (
+    <>
+      <form>
+        <label>
+          Greeting:
+          <input type="text" name="name"></input>
+        </label>
+        <button type="submit">Some text just too</button>
+      </form>
+    </>
+  );
+}
