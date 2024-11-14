3d1381cf41a74b063af9cc66fcda0c7896ea994f
Sabine <sabinehernandes@gmail.com>
2024-11-14 16:18:38 +0100

    cors git commit -m “$1
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- chili/src/main/java/gcp/chili/CanineController.java
+++ chili/src/main/java/gcp/chili/CanineController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin
 @RequestMapping("/canines")
 public class CanineController {
     CanineRepository repo;
